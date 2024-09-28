5315e10716bd02a2644f7fc7035a398e27c47497
macsimir <darknezzz192@gmail.com>
2024-09-29 00:21:15 +0300

    vault backup: 2024-09-29 00:21:15

--- django-theory/1create_templates/apps/views.py
+++
@@ -1,6 +0,0 @@
-#views.py
-from django.shortcuts import render #Имопртируем функцию для рендера html файла 
-
-def home(request):
-    """Функция home для вывода страницы home.html"""
-    return render(request,"home.html")
